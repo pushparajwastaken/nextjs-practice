d152367de5795575cca2c96ee208073ea2ff51eb
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-02-01 22:02:35 +0530

    add fullstack project

---
+++ fullstack/model/user.model.tsx
@@ -0,0 +1,62 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+export interface User extends Document {
+  userName: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  message: Message[];
+  createdAt: Date;
+}
+const UserSchema: Schema<User> = new Schema({
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verifyCode: {
+    type: String,
+    required: true,
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: true,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
