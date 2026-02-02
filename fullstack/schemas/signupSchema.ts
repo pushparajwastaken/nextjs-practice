0f1b459a134bfc6336774c9c8b0aaf5201bc3d8e
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-02-02 14:11:58 +0530

    modify signUpSchema

--- fullstack/schemas/signupSchema.ts
+++ fullstack/schemas/signupSchema.ts
@@ -8,4 +8,5 @@ export const userNameValidation = z
 export const signUpSchema = z.object({
   username: userNameValidation,
   email: z.email({ message: "Invalid email address" }),
+  password:z.string().min(6,{message:"Password must be atleast 6 characters"})
 });
