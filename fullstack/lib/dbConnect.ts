313924bdb56ba596793ae063c3bd29072cf96251
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-02-02 14:26:48 +0530

    add db connection

---
+++ fullstack/lib/dbConnect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+type ConnectionObjext = {
+  isConnected?: number;
+};
+const connection: ConnectionObjext = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to the database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB Connected Succesfully");
+  } catch (error) {
+    console.log("Database Connection Failed");
+
+    process.exit(1);
+  }
+}
+export default dbConnect;
