695d26c5588c28d87f106cb1cc16d945cc164d7c
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-02-20 20:55:15 +0530

    add login route

---
+++ nextauth/app/api/users/login/route.ts
@@ -0,0 +1,61 @@
+import { dbConnect } from "@/dbConfig/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+dbConnect();
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log("reqBody: ", reqBody);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Email doesn't exist",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    console.log("User exists");
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        {
+          error: "Incorrect Password",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    const tokenData = {
+      id: user.id,
+      username: user.username,
+    };
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json({
+      message: "Logged In Successfully",
+      success: true,
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
